f65d9884a2a0dc9bcb1286eaa2b8509aec80d843
Andy <ands_riss@hotmail.com>
2022-08-24 20:21:27 -0300

    primer entregable js

---
+++ code.js
@@ -0,0 +1,48 @@
+
+// 1er ejemplo==================================================================================================
+
+// let numero = parseInt(prompt(" Armarmos una lista?, decime el 1er numero que sea menor y no igual que el 2do"));
+// let numero1 = parseInt(prompt("decie el 2do numero"));
+// if (numero < numero1) {
+//     for (let i = numero; i <= numero1; i++) {
+//         if (i === 12) {
+//             continue
+//         }
+//         document.write(`lista de los numeros ` + i + `<br>`);
+//     }
+// }
+
+// else if (numero > numero1) {
+//     alert("ingresaste un numero menor que el primero")
+// }
+// else if (numero == numero1) {
+//     alert("ingresaste un numero igual")
+// }
+// else if (numero !== numero1) {
+//     alert("Ingresaste una letra, vuelva a poner un numero!")
+// }
+
+
+// 2do Ejemplo =====================================================================================================
+
+// let usuario = prompt("Ingrese su Usuario")
+// let calcular = parseInt(prompt(usuario + " Ingrese 1er Numero a Sumar la cuenta tiene que dar un resultado X mayor a 10"));
+// let calcular2 = parseInt(prompt("Ingrese 2do Numero a Sumar"));
+// do {
+//     let calcular = parseInt(prompt(usuario + " la suma es menor = a 10, tiene que ser mayor 10, por favor ingresa el 1er Numero"));
+//     let calcular2 = parseInt(prompt("Ingrese 2do Numero a Sumar"));
+//     resultado = calcular + calcular2;
+//     alert(resultado + " es mayor a 10");
+// } while (resultado <= 10);
+
+// 3er Ejemplo =======================================================================================================
+
+
+
+
+
+
+
+
+
+
